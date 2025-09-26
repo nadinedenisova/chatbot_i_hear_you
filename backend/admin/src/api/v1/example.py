06071ea74b32fb6ee667e9443a366e79dc26c2c3
1Croydan1 <gorshankov2015@ya.ru>
2025-09-26 18:12:55 +0300

    first try

--- backend/admin/src/api/v1/example.py
+++ backend/admin/src/api/v1/example.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from redis.asyncio import Redis
+#from redis.asyncio import Redis
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.db.postgres import get_async_session
-from src.db.redis import get_redis
+from db.postgres import get_async_session
+from db.redis import get_redis
 
 
 router = APIRouter()
@@ -15,7 +15,7 @@ router = APIRouter()
     summary='Базовый пример роутера'
 )
 async def base(
-    redis: Annotated[Redis, Depends(get_redis)],
+    #redis: Annotated[Redis, Depends(get_redis)],
     pg_session: Annotated[AsyncSession, Depends(get_async_session)]
 ):
     return {"Hello": "World"}
---
+++ backend/admin/src/schemas/entity.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, UUID4, Field
