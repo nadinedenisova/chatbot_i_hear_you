fc466db1189d05d41ba9cf9e49a4fe9f7c20715f
dimar <dimaremenec@gmail.com>
2025-09-27 10:30:14 +0300

    added User model

---
+++ backend/admin/src/models/users.py
@@ -0,0 +1,20 @@
+import uuid
+
+from sqlalchemy import text, String
+from sqlalchemy.orm import mapped_column, Mapped
+from src.db.postgres import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+        server_default=text('gen_random_uuid()'),
+        index=True,
+    )
+
+    phone_number: Mapped[str] = mapped_column(
+        String(30)
+    )
