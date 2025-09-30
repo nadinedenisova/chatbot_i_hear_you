d808170399a74abdd34b5d42a0560343bcf0a230
Zulfat-Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2025-09-30 19:13:37 +1000

    вынес в переменные API_TIMEOUT=10

--- backend/bot/src/config.py
+++ backend/bot/src/config.py
@@ -13,7 +13,7 @@ class Config:
 
     BOT_TOKEN = env.str('BOT_TOKEN')
     API_URL = env.str('API_URL')
-    API_TIMEOUT: int = 10  # Время ожидания ответа от API в секундах
+    API_TIMEOUT = env.int('API_TIMEOUT')  # Время ожидания ответа в секундах
     LOG_LEVEL = env.str('LOG_LEVEL', default='INFO')
 
 
